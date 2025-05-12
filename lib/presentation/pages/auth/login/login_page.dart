eb770cd1d04fb7a6b2a1317354947fc84572e81d
Hollman Rojas <hollmanerojas@gmail.com>
2025-05-12 12:57:32 -0500

    Feature/Add styles to loginPage

--- lib/presentation/pages/auth/login/login_page.dart
+++ lib/presentation/pages/auth/login/login_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -7,45 +6,118 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
+        body: SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      child: Stack(
+        alignment: Alignment.center,
         children: [
-          const Icon(
-            Icons.person,
-            color: Colors.black,
-            size: 125,
+          Image.asset(
+            'assets/img/background11.png',
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            fit: BoxFit.cover,
+            color: Colors.black54,
+            colorBlendMode: BlendMode.darken,
           ),
-          const Text(
-            'LOGIN',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
+          Container(
+            width: MediaQuery.of(context).size.width * 0.85,
+            height: MediaQuery.of(context).size.height * 0.75,
+            decoration: const BoxDecoration(
+              color: Color.fromRGBO(255, 255, 255, 0.3),
+              borderRadius: BorderRadius.all(
+                Radius.circular(25),
+              ),
             ),
-          ),
-          const TextField(
-            decoration: InputDecoration(label: Text('Correo Electrónico')),
-          ),
-          const TextField(
-            decoration: InputDecoration(label: Text('Contraseña')),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-            child: const Text(
-              'Iniciar Sesión',
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-          const Text('No tienes cuenta?'),
-          ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-            child: const Text(
-              'Registrate',
-              style: TextStyle(color: Colors.white),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.person,
+                  color: Colors.white,
+                  size: 125,
+                ),
+                const Text(
+                  'LOGIN',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 25, right: 25),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      label: Text(
+                        'Correo Electrónico',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 25, right: 25),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      label: Text(
+                        'Contraseña',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.only(left: 25, right: 25, top: 25),
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                    child: const Text(
+                      'Iniciar Sesión',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const Text('No tienes cuenta?'),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin:
+                      const EdgeInsets.only(left: 25, right: 25, bottom: 10),
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                    child: const Text(
+                      'Registrate',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
