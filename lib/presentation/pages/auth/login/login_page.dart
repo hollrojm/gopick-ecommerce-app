8d0548f80d9cf975543e291fef9a4a76dd4bd6f5
Hollman Rojas <hollmanerojas@gmail.com>
2025-05-12 12:05:35 -0500

    Feature/add login sketch

--- lib/presentation/pages/auth/login/login_page.dart
+++ lib/presentation/pages/auth/login/login_page.dart
@@ -1,14 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+    return Scaffold(
+        body: Container(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.person,
+            color: Colors.black,
+            size: 125,
+          ),
+          const Text(
+            'LOGIN',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const TextField(
+            decoration: InputDecoration(label: Text('Correo Electrónico')),
+          ),
+          const TextField(
+            decoration: InputDecoration(label: Text('Contraseña')),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+            child: const Text(
+              'Iniciar Sesión',
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          const Text('No tienes cuenta?'),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+            child: const Text(
+              'Registrate',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
       ),
-    );
+    ));
   }
 }
