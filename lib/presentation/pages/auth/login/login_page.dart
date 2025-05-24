c453f0855dc6da9361d8366d7c03552661e2dcd3
Hollman Rojas <hollmanerojas@gmail.com>
2025-05-23 22:46:55 -0500

    Feature/ add background into login screen

--- lib/presentation/pages/auth/login/login_page.dart
+++ lib/presentation/pages/auth/login/login_page.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
 import 'package:ecommerce_flutter/presentation/pages/auth/login/bloc/login_bloc_cubit.dart';
 import 'package:ecommerce_flutter/presentation/presentation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rive/rive.dart' as rive;
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -26,144 +29,150 @@ class _LoginPageState extends State<LoginPage> {
     _loginBlocCubit = BlocProvider.of<LoginBlocCubit>(context, listen: false);
 
     return Scaffold(
-        body: SizedBox(
-      width: double.infinity,
-      child: Stack(
+      body: Stack(
         alignment: Alignment.center,
         children: [
-          Image.asset(
-            'assets/img/background11.png',
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            fit: BoxFit.cover,
-            color: Colors.black54,
-            colorBlendMode: BlendMode.darken,
+          Positioned(
+            bottom: 200,
+            //height: 100,
+            left: 100,
+            width: MediaQuery.of(context).size.width * 1.7,
+            child: Image.asset('assets/Backgrounds/Spline.png'),
           ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.85,
-            height: MediaQuery.of(context).size.height * 0.75,
-            decoration: const BoxDecoration(
-              color: Color.fromRGBO(255, 255, 255, 0.3),
-              borderRadius: BorderRadius.all(
-                Radius.circular(25),
+          const rive.RiveAnimation.asset('assets/RiveAssets/shapes.riv'),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 30,
+                sigmaY: 30,
               ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Icon(
-                  Icons.person,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: MediaQuery.of(context).size.height * 0.75,
+                decoration: const BoxDecoration(
                   color: Colors.white,
-                  size: 125,
-                ),
-                const Text(
-                  'LOGIN',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25),
                   ),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(left: 25, right: 25),
-                  child: StreamBuilder(
-                      stream: _loginBlocCubit!.emailStream,
-                      builder: (context, snapshot) {
-                        return DefaultTextField(
-                          label: 'Correo Electrónico',
-                          icon: Icons.email,
-                          onChanged: (text) {
-                            _loginBlocCubit!.changeEmail(text);
-                          },
-                        );
-                      }),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(left: 25, right: 25),
-                  child: StreamBuilder(
-                      stream: _loginBlocCubit!.passwordStream,
-                      builder: (context, snapshot) {
-                        return DefaultTextField(
-                          obscureText: true,
-                          label: 'Contraseña',
-                          icon: Icons.lock,
-                          onChanged: (text) {
-                            _loginBlocCubit!.changePassword(text);
-                          },
-                        );
-                      }),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: const EdgeInsets.only(
-                      left: 25, right: 25, top: 25, bottom: 10),
-                  height: 50,
-                  child: StreamBuilder(
-                      stream: _loginBlocCubit!.validateForm,
-                      builder: (context, snapshot) {
-                        return ElevatedButton(
-                          onPressed: () {
-                            if (snapshot.hasData) {
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text('Error en el formulario'),
-                                ),
-                              );
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.green),
-                          child: const Text(
-                            'Iniciar Sesión',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        );
-                      }),
-                ),
-                Row(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Container(
-                      margin: const EdgeInsets.only(left: 5, right: 5),
-                      width: 65,
-                      height: 1,
+                    const Icon(
+                      Icons.person,
                       color: Colors.white,
+                      size: 125,
                     ),
                     const Text(
-                      'No tienes cuenta?',
-                      style: TextStyle(color: Colors.white, fontSize: 17),
+                      'LOGIN',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
                     Container(
-                      width: 65,
-                      height: 1,
-                      color: Colors.white,
+                      margin: const EdgeInsets.only(left: 25, right: 25),
+                      child: StreamBuilder(
+                          stream: _loginBlocCubit!.emailStream,
+                          builder: (context, snapshot) {
+                            return DefaultTextField(
+                              label: 'Correo Electrónico',
+                              icon: Icons.email,
+                              onChanged: (text) {
+                                _loginBlocCubit!.changeEmail(text);
+                              },
+                            );
+                          }),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 25, right: 25),
+                      child: StreamBuilder(
+                          stream: _loginBlocCubit!.passwordStream,
+                          builder: (context, snapshot) {
+                            return DefaultTextField(
+                              obscureText: true,
+                              label: 'Contraseña',
+                              icon: Icons.lock,
+                              onChanged: (text) {
+                                _loginBlocCubit!.changePassword(text);
+                              },
+                            );
+                          }),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(
+                          left: 25, right: 25, top: 25, bottom: 10),
+                      height: 50,
+                      child: StreamBuilder(
+                          stream: _loginBlocCubit!.validateForm,
+                          builder: (context, snapshot) {
+                            return ElevatedButton(
+                              onPressed: () {
+                                if (snapshot.hasData) {
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text('Error en el formulario'),
+                                    ),
+                                  );
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.green),
+                              child: const Text(
+                                'Iniciar Sesión',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            );
+                          }),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(left: 5, right: 5),
+                          width: 65,
+                          height: 1,
+                          color: Colors.white,
+                        ),
+                        const Text(
+                          'No tienes cuenta?',
+                          style: TextStyle(color: Colors.white, fontSize: 17),
+                        ),
+                        Container(
+                          width: 65,
+                          height: 1,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(
+                          left: 25, right: 25, top: 10, bottom: 10),
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, 'register');
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black),
+                        child: const Text(
+                          'Registrate',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
                     ),
                   ],
                 ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: const EdgeInsets.only(
-                      left: 25, right: 25, top: 10, bottom: 10),
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'register');
-                    },
-                    style:
-                        ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                    child: const Text(
-                      'Registrate',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ],
       ),
-    ));
+    );
   }
 }
